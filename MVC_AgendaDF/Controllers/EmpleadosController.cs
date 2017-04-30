aa05400344603bd09064b280cb65b8edf650b3a8
Georgina Abellán Villar <geor.abellan@gmail.com>
2017-04-30 20:19:36 +0200

    Carpetas Modelo, Vista y Controlador

---
+++ MVC_AgendaDF/Controllers/EmpleadosController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MvcAgendaDF.Models;
+
+namespace MvcAgendaDF.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        private AgendaEntities db = new AgendaEntities();
+
+        // GET: Empleados
+        public ActionResult Index()
+        {
+            var empleado = db.Empleado.Include(e => e.Departamento);
+            return View(empleado.ToList());
+        }
+
+        // GET: Empleados/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleado empleado = db.Empleado.Find(id);
+            if (empleado == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empleado);
+        }
+
+        // GET: Empleados/Create
+        public ActionResult Create()
+        {
+            ViewBag.Departamento_Id = new SelectList(db.Departamento, "Id", "Nombre");
+            return View();
+        }
+
+        // POST: Empleados/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nombre,FechaNacimiento,Departamento_Id")] Empleado empleado)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Empleado.Add(empleado);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Departamento_Id = new SelectList(db.Departamento, "Id", "Nombre", empleado.Departamento_Id);
+            return View(empleado);
+        }
+
+        // GET: Empleados/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleado empleado = db.Empleado.Find(id);
+            if (empleado == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Departamento_Id = new SelectList(db.Departamento, "Id", "Nombre", empleado.Departamento_Id);
+            return View(empleado);
+        }
+
+        // POST: Empleados/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nombre,FechaNacimiento,Departamento_Id")] Empleado empleado)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(empleado).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Departamento_Id = new SelectList(db.Departamento, "Id", "Nombre", empleado.Departamento_Id);
+            return View(empleado);
+        }
+
+        // GET: Empleados/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleado empleado = db.Empleado.Find(id);
+            if (empleado == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empleado);
+        }
+
+        // POST: Empleados/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Empleado empleado = db.Empleado.Find(id);
+            db.Empleado.Remove(empleado);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MVC_AgendaDF/Controllers/TareasController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MvcAgendaDF.Models;
+
+namespace MvcAgendaDF.Controllers
+{
+    public class TareasController : Controller
+    {
+        private AgendaEntities db = new AgendaEntities();
+
+        // GET: Tareas
+        public ActionResult Index()
+        {
+            var tarea = db.Tarea.Include(t => t.Empleado);
+            return View(tarea.ToList());
+        }
+
+        // GET: Tareas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tarea tarea = db.Tarea.Find(id);
+            if (tarea == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tarea);
+        }
+
+        // GET: Tareas/Create
+        public ActionResult Create()
+        {
+            ViewBag.Empleado_Id = new SelectList(db.Empleado, "Id", "Nombre");
+            return View();
+        }
+
+        // POST: Tareas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Descripcion,FechaInicio,FechaFin,HoraInicio,HoraFin,Empleado_Id")] Tarea tarea)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tarea.Add(tarea);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Empleado_Id = new SelectList(db.Empleado, "Id", "Nombre", tarea.Empleado_Id);
+            return View(tarea);
+        }
+
+        // GET: Tareas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tarea tarea = db.Tarea.Find(id);
+            if (tarea == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Empleado_Id = new SelectList(db.Empleado, "Id", "Nombre", tarea.Empleado_Id);
+            return View(tarea);
+        }
+
+        // POST: Tareas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Descripcion,FechaInicio,FechaFin,HoraInicio,HoraFin,Empleado_Id")] Tarea tarea)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tarea).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Empleado_Id = new SelectList(db.Empleado, "Id", "Nombre", tarea.Empleado_Id);
+            return View(tarea);
+        }
+
+        // GET: Tareas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tarea tarea = db.Tarea.Find(id);
+            if (tarea == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tarea);
+        }
+
+        // POST: Tareas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tarea tarea = db.Tarea.Find(id);
+            db.Tarea.Remove(tarea);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
